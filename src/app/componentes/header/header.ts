b639d2055b3b7858a111ff9821563ffb13a74855
Jesús García <manzano.21jgh@gmail.com>
2026-07-17 12:41:18 -0600

    nueva validacion para que el carrito este vacio antes de cambiar de perfil

--- src/app/componentes/header/header.ts
+++ src/app/componentes/header/header.ts
@@ -82,6 +82,18 @@ export class HeaderComponent {
   }
 
 async confirmarCambioRol() {
+
+// NUEVA VALIDACIÓN: Revisamos si el carrito tiene al menos 1 producto
+    if (this.cartService.carrito().length > 0) {
+      this.snackBar.open('Para poder cambiar de perfil, necesitas vaciar tu carrito de compras.', 'Entendido', {
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        panelClass: ['alerta-carrito']
+      });
+      return; // Detenemos la función aquí mismo
+    }
+    
     const user = this.authService.currentUser();
     const userData = this.authService.userData(); // Obtener la data actual
  
