bbfef7eecc5950aa63e82e8cf9a14f69cde63386
Jesús García <manzano.21jgh@gmail.com>
2026-07-17 16:43:44 -0600

    configuracion de  cambio de perfil si carrito esta vacion, esto en el footer

--- src/app/componentes/footer/footer.ts
+++ src/app/componentes/footer/footer.ts
@@ -3,10 +3,12 @@ import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EditarImagenesFHComponent } from "./../editar-imagenes-fh/editar-imagenes-fh";
+import { CartService } from '../../services/cart/cart-service'
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-footer',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatSnackBarModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
@@ -14,6 +16,8 @@ export class FooterComponent {
   constructor() { }
   authService = inject(Firebase)
   usuario: any = ""
+  cartService = inject(CartService);
+  private snackBar = inject(MatSnackBar);
 
   private dialog = inject(MatDialog);
   async ngOnInit() {
@@ -33,20 +37,32 @@ export class FooterComponent {
     }
   }
 
-  async confirmarCambioRol() {
-    const user = this.authService.currentUser();
-    if (!user) return; // O muestra una alerta
+async confirmarCambioRol() {
 
-    const rolActual = this.authService.userData()?.rol;
-    try {
-      // Aquí puedes llamar a tu modal de confirmación antes de ejecutar
-      const nuevoRol = await this.authService.cambiarRolUsuario(user.uid, rolActual);
-
-      console.log("Cambio exitoso a:", nuevoRol);
-      // Aquí puedes mostrar un Toast o SnackBar de éxito
-    } catch (error) {
-      console.error("Error al cambiar:", error);
+// NUEVA VALIDACIÓN: Revisamos si el carrito tiene al menos 1 producto
+    if (this.cartService.carrito().length > 0) {
+      this.snackBar.open('Para poder cambiar de perfil, necesitas vaciar tu carrito de compras.', 'Entendido', {
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        panelClass: ['alerta-carrito']
+      });
+      return; // Detenemos la función aquí mismo
     }
+    
+    const user = this.authService.currentUser();
+    const userData = this.authService.userData(); // Obtener la data actual
+ 
+    // Validar que exista el usuario y que los datos de Firestore ya estén cargados
+    if (!user || !userData) {
+      console.error("Aún no se ha cargado el usuario o sus datos de Firestore.");
+      return;
+    }
+
+    console.log("ID DEL USUARIO EN EL HEADER:", user.uid);
+
+    const rolActual = userData.rol;
+    await this.authService.cambiarRolUsuario(user.uid, rolActual);
   }
 
 }
